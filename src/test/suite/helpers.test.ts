2a9d962444f9fdce8b9d22887ca374e0b7aa14a0
Michael Stocks <michaelstocks1@gmail.com>
2023-05-08 13:25:55 +0200

    Included some tests but not run yet

---
+++ src/test/suite/helpers.test.ts
@@ -0,0 +1,57 @@
+import * as assert from 'assert';
+
+import {convertFromJson, convertFromYaml} from "../../helpers";
+import { loadFixture, stripNewLines } from '../testHelpers';
+import { NamingConvention } from '../../converter';
+
+suite('helpers', () => {
+    suite('convertFromJson', () => {
+        test('should convert json to camel case', async () => {
+            const [input, expected] = await Promise.all([
+                loadFixture('input.json'),
+                loadFixture('expectedCamelJson.json')
+            ]);
+
+            const result = convertFromJson(input, NamingConvention.Camel);
+
+            assert.deepStrictEqual(stripNewLines(result), stripNewLines(expected));
+        });
+
+        test('should convert json to snake case', async () => {
+            const [input, expected] = await Promise.all([
+                loadFixture('input.json'),
+                loadFixture('expectedSnakeJson.json')
+            ]);
+
+            const result = convertFromJson(input, NamingConvention.Snake);
+
+            assert.deepStrictEqual(stripNewLines(result), stripNewLines(expected));
+        });
+    });
+
+    suite('convertFromYaml', () => {
+        test('should convert yaml to kebab case', async () => {
+            const [input, expected] = await Promise.all([
+                loadFixture('input.yaml'),
+                loadFixture('expectedKebabYaml.yaml')
+            ]);
+
+            const result = convertFromYaml(input, NamingConvention.Kebab);
+
+            assert.deepStrictEqual(stripNewLines(result), stripNewLines(expected));
+        });
+
+        test('should convert yaml to constant case', async () => {
+            const [input, expected] = await Promise.all([
+                loadFixture('input.yaml'),
+                loadFixture('expectedConstantYaml.yaml')
+            ]);
+
+            const result = convertFromYaml(input, NamingConvention.Constant);
+
+            assert.deepStrictEqual(stripNewLines(result), stripNewLines(expected));
+        });
+    });
+
+
+});
